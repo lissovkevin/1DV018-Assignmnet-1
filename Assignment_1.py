b0f4466dd61f04a4a8fbd01ade1982894c537435
Kevin Lissov <kl223py@student.lnu.se>
2026-09-21 11:17:41 +0200

    .

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,5 +1,5 @@
 import random
-
+import time
 
 def threesum_brute(lst, sum=0):
     sumZero = []
@@ -28,11 +28,30 @@ def threesum_cache(lst, sum=0):
     return sumZero
 
 
-for _ in range(3):
-    n = 15
-    random_lst = []
-    for _ in range(n):
-        random_lst.append(random.randint(-10 * n, 10 * n))
-    print(random_lst)
-    print(threesum_brute(random_lst))
-    print(threesum_cache(random_lst))
+brute_sizes = list(range(200, 696, 35))
+brute_times = []
+for n in brute_sizes:
+    brute_times.append([])
+    for _ in range(3):
+        random_lst = []
+        for _ in range(n):
+            random_lst.append(random.randint(-10 * n, 10 * n))
+        start = time.time()
+        result = threesum_brute(random_lst)
+        end = time.time()
+        brute_times[-1].append(end - start)
+        print(f'{end - start:.10f}')
+
+cache_sizes = list(range(950, 2476, 108))
+cache_times = []
+for n in cache_sizes:
+    cache_times.append([])
+    for _ in range(3):
+        random_lst = []
+        for _ in range(n):
+            random_lst.append(random.randint(-10 * n, 10 * n))
+        start = time.time()
+        result = threesum_cache(random_lst)
+        end = time.time()
+        cache_times[-1].append(end - start)
+        print(f'{end - start:.10f}')
